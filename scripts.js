f7acde5fbe0622514ce767276edb35cf1020a1b9
Ɖ4₦₮3_Ɖ3Ꝟ <d4nt3.d3v@gmail.com>
2023-04-06 02:47:45 -0500

    update

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.write('<script src="https://d4nt3d3v.github.io/assets/SocialNetworks_Panel/scripts.js"></script>');
+var Social_Discord="aaaaaa";
+var Social_YouTube="";
+var Social_GitHub="";
+var Social_WhatsApp="aaa";
+var Social_Telegram="";
+var Social_Facebook="";
+var Social_LinkedIn="";
+var Social_Twitter="aaa";
+var Social_Twitch="";
+var Social_Instagram="";
+var Social_TikTok="aaaa";

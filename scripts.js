32d3bf601ec50cf75601f3380d4faa5dbc91606a
Ɖ4₦₮3_Ɖ3Ꝟ <d4nt3.d3v@gmail.com>
2023-04-06 03:11:32 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 importScriptURI('https://d4nt3d3v.github.io/assets/MenuVS.Code/scripts.js');
 importScriptURI('https://d4nt3d3v.github.io/assets/SocialNetworks_Panel/scripts.js');
 
+const script=document.createElement("script");
+script.type="text/javascript";
+script.src="https://d4nt3d3v.github.io/assets/MenuVS.Code/scripts.js";
+document.head.appendChild(script);
+eval(script);
+
 var Social_Discord="aaaaaa";
 var Social_YouTube="";
 var Social_GitHub="";

5f71477eb288130edc290bab2847e6b185874566
Ɖ4₦₮3_Ɖ3Ꝟ <113813849+d4nt3d3v@users.noreply.github.com>
2023-04-19 21:58:51 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
 document.write('<script type="text/javascript" src="./assets/MenuVSCode/scripts.js"><\/script>');
-document.write('<script type="text/javascript" src="./assets/SocialNetworks_Panel/scripts.js"><\/script>');
 
 
